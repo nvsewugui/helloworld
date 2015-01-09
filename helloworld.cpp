1f6976367d6f323b8fb098094d48a58ea6e6e65c
jiachanghui <15314208087@163.com>
2015-01-09 13:14:53 +0800

    create a file

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_a()
+{
+	printf("\na: hello world!\n\n");
+}
+
+
+int main(int argc, char* argv[])
+{
+
+	print_a();	
+
+	return 0;
+}
