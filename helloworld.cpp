1189d1a456687ea703429ee6de5dad0927b9b2c2
jiachanghui <jiachanghui@emar.com>
2015-01-09 13:17:30 +0800

    add a function

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,11 +7,16 @@ void print_a()
 	printf("\na: hello world!\n\n");
 }
 
+void print_b()
+{
+	printf("\nb: hello world!\n\n");
+}
 
 int main(int argc, char* argv[])
 {
 
 	print_a();	
+	print_b();	
 
 	return 0;
 }
